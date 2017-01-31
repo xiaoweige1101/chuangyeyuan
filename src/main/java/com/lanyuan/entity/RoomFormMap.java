fea00f7ae536f648f1f09d85663ebd94630afb0f
xiaoweige1101 <zhangwei_1101@126.com>
2017-01-31 13:33:47 +0800

    add sql

---
+++ src/main/java/com/lanyuan/entity/RoomFormMap.java
@@ -0,0 +1,15 @@
+package com.lanyuan.entity;
+
+import com.lanyuan.annotation.TableSeg;
+import com.lanyuan.util.FormMap;
+
+
+
+/**
+ * 实体表
+ */
+@TableSeg(tableName = "ly_room", id="id")
+public class RoomFormMap extends FormMap<String,Object>{
+	private static final long serialVersionUID = 1L;
+
+}

5f053adecce550526db2578f4f76d7b8d987cf8d
xiaoweige1101 <zhangwei_1101@126.com>
2017-01-31 14:47:59 +0800

    commit

--- src/main/java/com/lanyuan/controller/system/ManageContorller.java
+++ src/main/java/com/lanyuan/controller/system/ManageContorller.java
@@ -5,40 +5,24 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lanyuan.mapper.LogMapper;
 import com.lanyuan.controller.index.BaseController;
-import com.lanyuan.entity.LogFormMap;
-import com.lanyuan.plugin.PageView;
+import com.lanyuan.mapper.RoomMapper;
 import com.lanyuan.util.Common;
 
 @Controller
-@RequestMapping("/manage/")
+@RequestMapping("/manage")
 public class ManageContorller extends BaseController {
+	
 	@Inject
-	private LogMapper logMapper;
+	private RoomMapper roomMapper;
 
-	@RequestMapping("list")
-	public String listUI(Model model) throws Exception {
-		return Common.BACKGROUND_PATH + "/system/log/list";
+	@RequestMapping("/room/list")
+	public String list(Model model) throws Exception {
+		model.addAttribute("res", findByRes());
+		return Common.BACKGROUND_PATH + "/system/manage/list";
 	}
-
-	@ResponseBody
-	@RequestMapping("findByPage")
-	public PageView findByPage( String pageNow,
-			String pageSize,String column,String sort) throws Exception {
-		LogFormMap logFormMap = getFormMap(LogFormMap.class);
-		String order = "";
-		if(Common.isNotEmpty(column)){
-			order = " order by "+column+" "+sort;
-		}else{
-			order = " order by id asc";
-		}
-		
-		logFormMap.put("$orderby", order);
-		logFormMap=toFormMap(logFormMap, pageNow, pageSize,logFormMap.getStr("orderby"));
-        pageView.setRecords(logMapper.findByPage(logFormMap));
-		return pageView;
-	}
+	
+	
+	
 }

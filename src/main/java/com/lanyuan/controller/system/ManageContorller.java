cd1ce705ccd9e076d2250e261f7e3bb1984d6525
xiaoweige1101 <zhangwei_1101@126.com>
2017-03-04 15:11:23 +0800

    commit

--- src/main/java/com/lanyuan/controller/system/ManageContorller.java
+++ src/main/java/com/lanyuan/controller/system/ManageContorller.java
@@ -3,10 +3,16 @@ package com.lanyuan.controller.system;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lanyuan.annotation.SystemLog;
 import com.lanyuan.controller.index.BaseController;
+import com.lanyuan.entity.ResUserFormMap;
+import com.lanyuan.entity.UserFormMap;
+import com.lanyuan.entity.UserGroupsFormMap;
 import com.lanyuan.mapper.RoomMapper;
 import com.lanyuan.util.Common;
 
@@ -16,6 +22,32 @@ public class ManageContorller extends BaseController {
 	
 	@Inject
 	private RoomMapper roomMapper;
+	
+	/**********大楼管理*********/
+	@RequestMapping("/building/list")
+	public String listBuilding(Model model) throws Exception {
+		model.addAttribute("res", findByRes());
+		return Common.BACKGROUND_PATH + "/system/manage/building/list";
+	}
+	
+	@RequestMapping("/building/addUI")
+	public String addUI(Model model) throws Exception {
+		return Common.BACKGROUND_PATH + "/system/manage/building/add";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/building/deleteEntity")
+	@Transactional(readOnly=false)//需要事务操作必须加入此注解
+	@SystemLog(module="系统管理",methods="用户管理-删除用户")//凡需要处理业务逻辑的.都需要记录操作日志
+	public String deleteEntity() throws Exception {
+		String[] ids = getParaValues("ids");
+		for (String id : ids) {
+//			userMapper.deleteByAttribute("userId", id, UserGroupsFormMap.class);
+//			userMapper.deleteByAttribute("userId", id, ResUserFormMap.class);
+//			userMapper.deleteByAttribute("id", id, UserFormMap.class);
+		}
+		return "success";
+	}
 
 	/**********房间管理************/
 	@RequestMapping("/room/list")
---
+++ src/main/webapp/js/system/manage/building/list.js
@@ -0,0 +1,120 @@
+var pageii = null;
+var grid = null;
+$(function() {
+	grid = lyGrid({
+		id : 'paging',
+		l_column : [ {
+			colkey : "id",
+			name : "id",
+			width : "50px",
+			hide : true
+		}, {
+			colkey : "roomNum",
+			name : "房间号"
+		}, {
+			colkey : "beginDate",
+			name : "入住日期",
+			width : "100px",
+		}, {
+			colkey : "roomRate",
+			name : "房租"
+		}, {
+			colkey : "returnableDeposit",
+			name : "押金"
+		}, {
+			colkey : "waterRecord",
+			name : "水费"
+		}, {
+			colkey : "electricRecord",
+			name : "电费"
+		}, {
+			colkey : "networkRecord",
+			name : "网费"
+		}, {
+			colkey : "contractPeriod",
+			name : "合同期"
+		}, {
+			colkey : "description",
+			name : "备注"
+		}, {
+			colkey : "nextChargeDate",
+			name : "下次交房租日期"
+		}
+		],
+		jsonUrl : rootPath + '/role/findByPage.shtml',
+		dymCol:true,
+		checkbox : true
+	});
+	$("#search").click("click", function() {// 绑定查询按扭
+		var searchParams = $("#searchForm").serializeJson();// 初始化传参数
+		grid.setOptions({
+			data : searchParams
+		});
+	});
+	$("#addRole").click("click", function() {
+		addRole();
+	});
+	$("#editRole").click("click", function() {
+		editRole();
+	});
+	$("#delRole").click("click", function() {
+		delRole();
+	});
+	$("#permissions").click("click", function() {
+		permissions();
+	});
+});
+function editRole() {
+	var cbox = grid.getSelectedCheckbox();
+	if (cbox.length > 1 || cbox == "") {
+		layer.msg("只能选中一个");
+		return;
+	}
+	pageii = layer.open({
+		title : "编辑",
+		type : 2,
+		area : [ "600px", "60%" ],
+		content : rootPath + '/role/editUI.shtml?id=' + cbox
+	});
+}
+function permissions() {
+	var cbox = grid.getSelectedCheckbox();
+	if (cbox.length > 1 || cbox == "") {
+		layer.msg("请选择一个对象！");
+		return;
+	}
+	var url = rootPath + '/resources/permissions.shtml?roleId='+cbox;
+	pageii = layer.open({
+		title : "分配权限",
+		type : 2,
+		area : [ "700px", "60%" ],
+		content : url
+	});
+}
+function addRole() {
+	pageii = layer.open({
+		title : "新增",
+		type : 2,
+		area : [ "600px", "60%" ],
+		content : rootPath + '/role/addUI.shtml'
+	});
+}
+function delRole() {
+	var cbox = grid.getSelectedCheckbox();
+	if (cbox == "") {
+		layer.msg("请选择删除项！！");
+		return;
+	}
+	layer.confirm('是否删除？', function(index) {
+		var url = rootPath + '/role/deleteEntity.shtml';
+		var s = CommnUtil.ajax(url, {
+			ids : cbox.join(",")
+		}, "json");
+		if (s == "success") {
+			layer.msg('删除成功');
+			grid.loadData();
+		} else {
+			layer.msg('删除失败');
+		}
+	});
+}

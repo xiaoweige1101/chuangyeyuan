ae70063e059e1c6c8080a2839ba42ea71e48f025
xiaoweige1101 <zhangwei_1101@126.com>
2017-10-09 09:33:46 +0800

    添加OTT灰度升级

---
+++ src/main/java/com/lanyuan/entity/chuangyeyuan/C_roomFormMap.java
@@ -0,0 +1,11 @@
+package com.lanyuan.entity.chuangyeyuan;
+
+import com.lanyuan.annotation.TableSeg;
+import com.lanyuan.util.FormMap;
+
+@TableSeg(tableName = "c_budget", id="id")
+public class C_roomFormMap extends FormMap<String,Object>{
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/lanyuan/mapper/chuangyeyuan/C_room_statusMapper.java
@@ -0,0 +1,7 @@
+package com.lanyuan.mapper.chuangyeyuan;
+
+import com.lanyuan.mapper.base.BaseMapper;
+
+public interface C_room_statusMapper extends BaseMapper {
+
+}
